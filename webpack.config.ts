8da5dab054f4adae2ee5c89cb9d0334130010125
Galen <galen.kistler@grafana.com>
2025-03-13 11:38:32 -0500

    chore: revert debug change

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,7 +2,7 @@ import LiveReloadPlugin from 'webpack-livereload-plugin';
 import type { Configuration } from 'webpack';
 import { merge } from 'webpack-merge';
 import grafanaConfig from './.config/webpack/webpack.config';
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const config = async (env: any): Promise<Configuration> => {
   const baseConfig = await grafanaConfig(env);
@@ -12,14 +12,14 @@ const config = async (env: any): Promise<Configuration> => {
       asyncWebAssembly: true,
     },
     plugins: [
-      new BundleAnalyzerPlugin()
-      // new LiveReloadPlugin({
-      //   port: 35828,
-      //   protocol: 'http',
-      //   hostname: 'localhost',
-      //   delay: 1000,
-      //   appendScriptTag: true,
-      // }),
+      // new BundleAnalyzerPlugin()
+      new LiveReloadPlugin({
+        port: 35828,
+        protocol: 'http',
+        hostname: 'localhost',
+        delay: 1000,
+        appendScriptTag: true,
+      }),
     ],
   });
 };
