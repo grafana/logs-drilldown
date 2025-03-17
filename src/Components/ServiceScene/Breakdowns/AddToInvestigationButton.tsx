ddf9166822801e248c3ecb14bc306e276c186705
Sven Grossmann <svennergr@gmail.com>
2025-03-17 13:06:59 +0100

    feat(investigations): dedupe fieldconfig

--- src/Components/ServiceScene/Breakdowns/AddToInvestigationButton.tsx
+++ src/Components/ServiceScene/Breakdowns/AddToInvestigationButton.tsx
@@ -99,13 +99,27 @@ export class AddToInvestigationButton extends SceneObjectBase<AddToInvestigation
               id: key,
               value: field.config[key as keyof FieldConfig],
             }));
-            fieldConfig.overrides.push({
-              matcher: {
-                id: 'byName',
-                options: field.config.displayNameFromDS ?? field.config.displayName ?? field.name,
-              },
-              properties,
-            });
+
+            // check if the override already exists
+            const existingOverride = fieldConfig.overrides.find(
+              (o) =>
+                o.matcher.options === (field.config.displayNameFromDS ?? field.config.displayName ?? field.name) &&
+                o.matcher.id === 'byName'
+            );
+            if (!existingOverride) {
+              // add as first override
+              fieldConfig.overrides.unshift({
+                matcher: {
+                  id: 'byName',
+                  options: field.config.displayNameFromDS ?? field.config.displayName ?? field.name,
+                },
+                properties,
+              });
+            }
+
+            if (existingOverride && JSON.stringify(existingOverride.properties) !== JSON.stringify(properties)) {
+              existingOverride.properties = properties;
+            }
           }
         }
       }
