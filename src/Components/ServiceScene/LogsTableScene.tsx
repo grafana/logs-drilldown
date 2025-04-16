2d440e246e45014721b1592ec72734c5c8aee9c0
L2D2Grafana <liza.detrick@grafana.com>
2025-04-16 15:55:32 -0700

    feat: (LogPanelTable) - add narrow func

--- src/Components/ServiceScene/LogsTableScene.tsx
+++ src/Components/ServiceScene/LogsTableScene.tsx
@@ -15,6 +15,7 @@ import { PanelMenu } from '../Panels/PanelMenu';
 import { setDisplayedFields } from '../../services/store';
 import { LogLineState } from '../Table/Context/TableColumnsContext';
 import { DEFAULT_URL_COLUMNS } from '../Table/constants';
+import { narrowUrlParam } from 'services/narrowing';
 
 let defaultUrlColumns = DEFAULT_URL_COLUMNS;
 
@@ -50,7 +51,7 @@ export class LogsTableScene extends SceneObjectBase<LogsTableSceneState> {
   onActivateSyncDisplayedFieldsWithUrlColumns = () => {
     const searchParams = new URLSearchParams(locationService.getLocation().search);
     const urlColumnsParam = searchParams.get('urlColumns');
-    const urlColumnsUrl = urlColumnsParam ? (JSON.parse(decodeURIComponent(urlColumnsParam)) as string[]) : null;
+    const urlColumnsUrl = narrowUrlParam(urlColumnsParam);
     const parentModel = this.getParentScene();
     // Sync from url
     defaultUrlColumns = urlColumnsUrl
