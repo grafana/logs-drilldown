88fba4a2b3e0fce36e70e1a145bf4da8268c7f76
Galen <galen.kistler@grafana.com>
2025-05-01 10:22:57 -0500

    chore: clean-up

--- src/Components/ServiceScene/LineFilter/LineFilterEditor.tsx
+++ src/Components/ServiceScene/LineFilter/LineFilterEditor.tsx
@@ -32,7 +32,6 @@ export function LineFilterEditor({
   setFocus,
   type,
 }: LineFilterEditorProps) {
-  console.log('LineFilterEditor', focus);
   const styles = useStyles2((theme) => getStyles(theme, type));
   const [width, setWidth] = useState(INITIAL_INPUT_WIDTH);
 
