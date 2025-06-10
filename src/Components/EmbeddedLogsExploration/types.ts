3cb7b6c94ee6f6c6082129555afe8c322414cb96
Galen <galen.kistler@grafana.com>
2025-06-10 08:10:02 -0500

    chore: clean up embedded prop definitions

--- src/Components/EmbeddedLogsExploration/types.ts
+++ src/Components/EmbeddedLogsExploration/types.ts
@@ -3,11 +3,22 @@ import { SceneTimeRangeState } from '@grafana/scenes';
 
 import { IndexSceneState } from 'Components/IndexScene/IndexScene';
 
-interface EmbeddedLogsExplorationFromQuery extends IndexSceneState {
-  datasourceUid?: string;
+interface EmbeddedLogsCommonProps extends IndexSceneState {
   onTimeRangeChange?: (timeRange: TimeRange) => void;
   query: string;
   timeRangeState: SceneTimeRangeState;
 }
 
-export type EmbeddedLogsExplorationProps = EmbeddedLogsExplorationFromQuery;
+// Datasource ID is required when embedded in another application
+interface EmbeddedLogsExplorationFromQuery extends IndexSceneState {
+  datasourceUid: string;
+}
+
+// But not required when testing, as we expect the datasource id to get pulled from the URL instead
+interface EmbeddedLogsExplorationTestingRoute extends IndexSceneState {
+  datasourceUid?: string;
+}
+
+export type EmbeddedLogsExplorationProps =
+  | (EmbeddedLogsExplorationFromQuery & EmbeddedLogsCommonProps)
+  | (EmbeddedLogsExplorationTestingRoute & EmbeddedLogsCommonProps);
