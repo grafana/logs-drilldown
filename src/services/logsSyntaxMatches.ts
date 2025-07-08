5d97040179c5b4f7041cdd7e843ba58538f1603e
Galen <galen.kistler@grafana.com>
2025-07-08 16:23:33 -0500

    chore: tweak regex

--- src/services/logsSyntaxMatches.ts
+++ src/services/logsSyntaxMatches.ts
@@ -10,9 +10,9 @@ export const logsSyntaxMatches: Record<string, RegExp> = {
   'log-token-trace': /(\b)(TRACE)($|\s)/gi,
 
   // Misc log markup regex
-  'log-token-key': /(\b|\B)[\w_]+(?=\s*=)/gi,
-  'log-token-method': /\b(GET|POST|PUT|DELETE|PATCH|HEAD|OPTIONS|TRACE|CONNECT)\b/g,
-  'log-token-size': /(?:\b|")\d+\.{0,1}\d*\s*[kKmMGgtTPp]*[bB]{1}(?:"|\b)/g,
   'log-token-uuid': /(\b|\B)[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}/g,
+  'log-token-method': /\b(GET|POST|PUT|DELETE|PATCH|HEAD|OPTIONS|TRACE|CONNECT)\b/gi,
+  'log-token-key': /(\b|\B)[\w_]+(?=\s*=)/gi,
+  'log-token-size': /(?:\b|")\d+\.{0,1}\d*\s*[kKmMGgtTPp]*[bB]{1}(?:"|\b)/g,
   'log-token-duration': /\b\d+(\.\d+)?(ns|µs|ms|s|m|h|d)\b/g,
 };
