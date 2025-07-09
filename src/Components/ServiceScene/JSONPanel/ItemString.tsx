68bbce5bb8873df41ec36c3e94682290a2fe16e9
Galen <galen.kistler@grafana.com>
2025-07-09 06:48:05 -0500

    chore: refactor interface

--- src/Components/ServiceScene/JSONPanel/ItemString.tsx
+++ src/Components/ServiceScene/JSONPanel/ItemString.tsx
@@ -5,14 +5,14 @@ import { hasProp } from '../../../services/narrowing';
 import { JsonDataFrameTimeName, JsonVizRootName } from '../LogsJsonScene';
 import { KeyPath } from '@gtk-grafana/react-json-tree/dist/types';
 
-interface GetItemStringProps {
+interface ItemStringProps {
   data: unknown;
   itemString: string;
   itemType: React.ReactNode;
   keyPath: KeyPath;
   nodeType: string;
 }
-export default function ItemString({ data, itemString, itemType, keyPath }: GetItemStringProps) {
+export default function ItemString({ data, itemString, itemType, keyPath }: ItemStringProps) {
   if (data && hasProp(data, JsonDataFrameTimeName) && typeof data.Time === 'string') {
     return null;
   }
