9ca7a205a245fd422d0bc7f04ddd6a2a90150b6d
L2D2Grafana <liza.detrick@grafana.com>
2025-07-22 17:14:20 -0700

    ci: hide certain commits remove comments

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,18 +5,7 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      [
-        'docs', // no changelog
-        'style', // no changelog
-        'refactor', // no changelog
-        'perf', // no changelog
-        'ci', // no changelog
-        'build', // no changelog
-        'revert', // no changelog
-        'wip', // no changelog - work in progress
-        'experimental', // no changelog - experimental features
-        'release', // no changelog - release commits
-      ],
+      ['docs', 'style', 'refactor', 'perf', 'ci', 'build', 'revert', 'wip', 'experimental', 'release'],
     ],
   },
 };
