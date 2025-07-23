a32321b18235f6017a332560097dbabc7e4ded6b
L2D2Grafana <liza.detrick@grafana.com>
2025-07-22 17:04:55 -0700

    ci: hide certain commits in the changelog

---
+++ .versionrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  types: [
+    { type: 'docs', section: 'Documentation', hidden: true },
+    { type: 'style', section: 'Styles', hidden: true },
+    { type: 'refactor', section: 'Code Refactoring', hidden: true },
+    { type: 'perf', section: 'Performance', hidden: true },
+    { type: 'ci', section: 'CI/CD', hidden: true },
+    { type: 'build', section: 'Build System', hidden: true },
+    { type: 'revert', section: 'Reverts', hidden: true },
+    { type: 'wip', section: 'Work in Progress', hidden: true },
+    { type: 'experimental', section: 'Experimental', hidden: true },
+    { type: 'release', section: 'Releases', hidden: true },
+  ],
+};
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Allow additional types that won't appear in changelogs
+    'type-enum': [
+      2,
+      'always',
+      [
+        'docs', // no changelog
+        'style', // no changelog
+        'refactor', // no changelog
+        'perf', // no changelog
+        'ci', // no changelog
+        'build', // no changelog
+        'revert', // no changelog
+        'wip', // no changelog - work in progress
+        'experimental', // no changelog - experimental features
+        'release', // no changelog - release commits
+      ],
+    ],
+  },
 };
