572dec464440e7c1e371125b61060d65c149df30
Matias Chomicki <matyax@gmail.com>
2025-07-23 16:09:12 +0200

    Remove console

--- src/services/analytics.ts
+++ src/services/analytics.ts
@@ -21,7 +21,6 @@ export const reportAppInteraction = (
     }
     sessionStorage.setItem(interactionName, '1');
   }
-  console.log(interactionName);
   reportInteraction(interactionName, properties);
 };
 
