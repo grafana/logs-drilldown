c18872a587df73fa9287080800937b95e4fc54a2
Galen <galen.kistler@grafana.com>
2025-07-30 11:01:42 -0500

    fix: show max series notice in panel

--- src/services/panel.ts
+++ src/services/panel.ts
@@ -16,6 +16,7 @@ import {
   PanelBuilders,
   QueryRunnerState,
   SceneDataProvider,
+  SceneDataProviderResult,
   SceneDataTransformer,
   SceneObject,
   SceneQueryRunner,
@@ -346,6 +347,20 @@ export function getQueryRunnerFromProvider(provider: SceneDataProvider): SceneQu
 
   throw new Error('SceneDataProvider is missing SceneQueryRunner');
 }
+
+export function setPanelNotices(result: SceneDataProviderResult, panel: VizPanel<{}, {}>) {
+  const frameWithNotice = result.data.series.find((df) => df.meta?.notices?.length);
+  if (frameWithNotice && frameWithNotice.meta?.notices?.length) {
+    panel.setState({
+      _pluginLoadError: frameWithNotice.meta?.notices[0].text,
+    });
+  } else if (panel.state._pluginLoadError) {
+    panel.setState({
+      _pluginLoadError: undefined,
+    });
+  }
+}
+
 export const logsControlsSupported =
   config.featureToggles.logsPanelControls &&
   (config.buildInfo.version > '12.1' || config.buildInfo.version.includes('12.1'));
