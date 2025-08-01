e44e21f75228646200453299b3d997e7b4353afb
Matias Chomicki <matyax@gmail.com>
2025-08-01 17:04:47 +0200

    chore: fix jest

--- src/services/combineResponsesWrapper.ts
+++ src/services/combineResponsesWrapper.ts
@@ -1,4 +1,4 @@
 // @ts-expect-error
 import Worker from 'comlink-loader!./combineResponsesWorker';
 
-export const combineResponsesWorker = new Worker();
+export const combineResponsesWorker = Worker ? new Worker() : undefined;
