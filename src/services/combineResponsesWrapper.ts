f9a67a605a403eca35e0c82ff98a8a7f202b7164
Matias Chomicki <matyax@gmail.com>
2025-08-01 17:23:19 +0200

    chore: extra safety

--- src/services/combineResponsesWrapper.ts
+++ src/services/combineResponsesWrapper.ts
@@ -1,4 +1,4 @@
 // @ts-expect-error
 import Worker from 'comlink-loader!./combineResponsesWorker';
 
-export const combineResponsesWorker = Worker ? new Worker() : undefined;
+export const CombineResponsesWorker = Worker ? Worker : undefined;
--- src/services/shardQuerySplitting.ts
+++ src/services/shardQuerySplitting.ts
@@ -6,11 +6,23 @@ import { DataFrame, DataQueryRequest, DataQueryResponse, LoadingState, QueryResu
 import { MaxSeriesRegex } from '../Components/ServiceScene/Breakdowns/QueryErrorAlert';
 import pluginJson from '../plugin.json';
 import { combineResponses } from './combineResponses';
-import { combineResponsesWorker } from './combineResponsesWrapper';
+import { CombineResponsesWorker as Worker } from './combineResponsesWrapper';
 import { logger } from './logger';
 import { addShardingPlaceholderSelector, getSelectorForShardValues, interpolateShardingSelector } from './logql';
 import { isValidQuery } from './logqlMatchers';
 import { LokiDatasource, LokiQuery } from './lokiQuery';
+
+// @ts-expect-error
+let combineResponsesWorker;
+try {
+  const instance = Worker();
+  if (instance) {
+    combineResponsesWorker = instance;
+  }
+} catch (e) {
+  logger.error(e);
+  combineResponsesWorker = undefined;
+}
 
 /**
  * Query splitting by stream shards.
