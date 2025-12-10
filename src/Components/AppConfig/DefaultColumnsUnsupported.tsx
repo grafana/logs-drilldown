63045972e72c0565a6669a33db133c32ce952a38
Galen <galen.kistler@grafana.com>
2025-12-10 09:24:44 -0600

    chore: fix eslint adding parans in the wrong spot

--- src/Components/AppConfig/DefaultColumnsUnsupported.tsx
+++ src/Components/AppConfig/DefaultColumnsUnsupported.tsx
@@ -6,13 +6,13 @@ export function DefaultColumnsUnsupported() {
   return (
     <section>
       <h2>Default columns is not supported on this instance of Grafana</h2>
-      {!config.featureToggles.kubernetesLogsDrilldown ||
-        (!config.featureToggles.grafanaAPIServerWithExperimentalAPIs && (
-          <p>
-            Default columns requires <code>kubernetesLogsDrilldown</code> and{' '}
-            <code>grafanaAPIServerWithExperimentalAPIs</code> feature flags to be enabled.
-          </p>
-        ))}
+      {(!config.featureToggles.kubernetesLogsDrilldown ||
+        !config.featureToggles.grafanaAPIServerWithExperimentalAPIs) && (
+        <p>
+          Default columns requires <code>kubernetesLogsDrilldown</code> and{' '}
+          <code>grafanaAPIServerWithExperimentalAPIs</code> feature flags to be enabled.
+        </p>
+      )}
       {config.buildInfo.version < '12.4' && <p>Default columns requires Grafana 12.4 or greater.</p>}
     </section>
   );
