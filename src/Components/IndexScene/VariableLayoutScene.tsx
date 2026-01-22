7f2daf42d38fcc8d33e7e09daa0323d2179e0d18
Matias Chomicki <matyax@gmail.com>
2026-01-22 16:51:07 +0100

    fix(VariableLayoutScene): add missing unit to width calc

--- src/Components/IndexScene/VariableLayoutScene.tsx
+++ src/Components/IndexScene/VariableLayoutScene.tsx
@@ -241,7 +241,7 @@ function getStyles(theme: GrafanaTheme2, height: number, headerCollapsed = false
       flexWrap: 'wrap',
       gap: theme.spacing(2),
       label: 'filtersWrap',
-      width: 'calc(100% - 450)',
+      width: 'calc(100% - 450px)',
     }),
     firstRowWrapper: css({
       '& > div > div': {
