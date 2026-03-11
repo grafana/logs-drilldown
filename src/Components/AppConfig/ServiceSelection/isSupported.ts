db84bb429a2f3766ae38b66ac3a679bf16ac250e
Matias Chomicki <matyax@gmail.com>
2026-03-11 15:33:19 +0100

    chore: update version

--- src/Components/AppConfig/ServiceSelection/isSupported.ts
+++ src/Components/AppConfig/ServiceSelection/isSupported.ts
@@ -2,7 +2,7 @@ import semver from 'semver/preload';
 
 import { config } from '@grafana/runtime';
 
-const DEFAULT_COLUMNS_MIN_VERSION = '12.4.0-20854440429';
+const DEFAULT_COLUMNS_MIN_VERSION = '13.0.0-22957488878';
 export const isDefaultLabelsVersionSupported = !semver.ltr(config.buildInfo.version, DEFAULT_COLUMNS_MIN_VERSION);
 export const isDefaultLabelsFlagsSupported = config.featureToggles.kubernetesLogsDrilldown;
 export const isDefaultLabelsSupported = isDefaultLabelsFlagsSupported && isDefaultLabelsVersionSupported;
