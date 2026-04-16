9335b9dfb3b1aae2abbc7781595e55ff81dec873
L2D2Grafana <liza.detrick@grafana.com>
2026-04-16 12:56:32 -0700

    chore(lint): spellcheck

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,4 +3,10 @@ const PRETTIER_WRITE = 'prettier --write --ignore-path .prettierignore';
 module.exports = {
   'src/**/*.{js,jsx,ts,tsx}': 'yarn lint',
   '**/*': ['eslint --fix', 'bash -c tsc-files --noEmit', PRETTIER_WRITE],
+  '*.{ts,tsx,js,go,md,mdx,yml,yaml,json,scss,css}': (filenames) => {
+    if (filenames.length === 0) {
+      return [];
+    }
+    return `cspell -c cspell.config.json --no-progress --no-summary ${filenames.join(' ')}`;
+  },
 };
