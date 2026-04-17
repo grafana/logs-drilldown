c3e9009eac349770aaff95b2b4f2a70eab6db14c
Nic Westvold <nic.westvold@grafana.com>
2026-04-17 16:30:08 -0400

    build: migrate package manager to pnpm

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 const PRETTIER_WRITE = 'prettier --write --ignore-path .prettierignore';
 
 module.exports = {
-  'src/**/*.{js,jsx,ts,tsx}': 'yarn lint',
+  'src/**/*.{js,jsx,ts,tsx}': 'pnpm run lint',
   '**/*': ['eslint --fix', 'bash -c tsc-files --noEmit', PRETTIER_WRITE],
 };
